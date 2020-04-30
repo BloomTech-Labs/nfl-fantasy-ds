75725224317ae481d803884366dc9f5ad2d63e0f
ezorigo <bzorigo@gmail.com>
2020-04-29 22:17:47 -0400

    added new feature to check the winning side of trade

---
+++ API/check_score.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def check_and_add(data):
+    winner = []
+    if data.iloc[0]['week1-pred'] > data.iloc[1]['week1-pred']:
+        winner.extend(['1','0'])
+    elif data.iloc[1]['week1-pred'] > data.iloc[0]['week1-pred']:
+        winner.extend(['0','1'])
+    else:
+        winner.extend(['0','0'])
+    data['winner'] = winner
+    return data
